19da795c63131f094be267b3efa8e21078ab7cad
Owais <owaisahmadqureshi019@gmail.com>
2025-05-07 02:56:02 +0500

    feat: implement responsive Navbar with theme toggle and drawer support
    - Added Navbar component with links to different sections of the app.
    - Introduced ModeToggle component for switching between light and dark themes.
    - Created ThemeProvider to manage theme state and persist it in local storage.
    - Developed reusable Button and Drawer components for UI consistency.
    - Updated App component to include Navbar and sections for content.
    - Refactored index.css to streamline styles and removed unnecessary rules.
    - Wrapped App in ThemeProvider for theme management.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import Navbar from '@/components/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <main>
+      <section className="w-full h-[90px] flex items-center bg-background text-foreground fixed opacity-95">
+        <Navbar />
+      </section>
+      <section id="#" className="pt-[95px]"></section>
+      <section id="about" className="pt-[95px]"></section>
+      <section id="skills" className="pt-[95px]"></section>
+      <section id="projects" className="pt-[95px]"></section>
+      <section id="contact-me" className="pt-[95px]"></section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { ArrowDownTrayIcon, Bars3Icon } from '@heroicons/react/16/solid';
+
+import { Button } from './ui/button';
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
+import { ModeToggle } from './mode-toggle';
+import { cn } from '@/lib/utils';
+
+const Navbar = () => {
+  const NavbarContent = () => {
+    const listClass =
+      'font-bold text-sm cursor-pointer transition-all duration-300 ease-in-out hover:scale-110 hover:text-primary hover:underline underline-offset-4';
+
+    return (
+      <>
+        <ul className="flex max-sm:flex-col items-center justify-center gap-10 max-sm:gap-5 max-md:pb-10">
+          <li className={listClass}>
+            <a href="#about">About Me</a>
+          </li>
+          <li className={cn(listClass)}>
+            <a href="#skills">Skills</a>
+          </li>
+          <li className={listClass}>
+            <a href="#projects">Projects</a>
+          </li>
+          <li className={listClass}>
+            <a href="#contact-me">Contact Me</a>
+          </li>
+        </ul>
+      </>
+    );
+  };
+
+  return (
+    <nav className="flex justify-between items-center w-[90%] mx-auto">
+      <div className="flex items-center gap-3">
+        <ModeToggle />
+        <p className="text-base cursor-pointer transition-all duration-300 ease-in-out hover:scale-105">
+          <strong>
+            <a href="#">Personal</a>
+          </strong>
+        </p>
+      </div>
+      <span className="max-sm:hidden">
+        <NavbarContent />
+      </span>
+      <Button className="max-sm:hidden">
+        Resume <ArrowDownTrayIcon />
+      </Button>
+      <div className="sm:hidden">
+        <Drawer>
+          <DrawerTrigger asChild>
+            <Bars3Icon className="h-[30px]" />
+          </DrawerTrigger>
+          <DrawerTitle></DrawerTitle>
+          <DrawerContent>
+            <NavbarContent />
+          </DrawerContent>
+        </Drawer>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
